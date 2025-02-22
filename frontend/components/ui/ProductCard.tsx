b050d44eefa28c7231f4d2ce188fab13493c3d23
FQTeo <fuqiangteo@gmail.com>
2025-02-22 17:16:04 +0800

    implement listings page with test data

--- frontend/components/ui/ProductCard.tsx
+++ frontend/components/ui/ProductCard.tsx
@@ -1,9 +1,20 @@
 'use client'
 
 import React from 'react';
+import { ProductCardInfo } from '@/types';
+import Image from 'next/image';
 
-export default function ProductCard() {
+export default function ProductCard(data:ProductCardInfo) {
     return (
-        <div></div>
+        <div className='h-[360px] w-[280px] p-3 rounded-xl bg-[#f8f8f8]'>
+            <div className='w-full h-[80%]'>
+                <Image src={data.image} height={1000} width={1000} alt={data.title}
+                    className='w-full h-full object-fill rounded-lg' />
+            </div>
+            <div className='mt-3 grid grid-cols-12 gap-4 text-black'>
+                <div className='col-span-7 font-bold line-clamp-2'>{data.title}</div>
+                <div className='col-span-5 text-right font-medium text-2xl'>${data.price}</div>
+            </div>
+        </div>
     )
 } 
---
+++ frontend/testdata.tsx
@@ -0,0 +1,55 @@
+export const testdata = [
+    {
+        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
+        "title": "Coldplay Vinyl",
+        "price": 60.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
+        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
+        "price": 55.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
+        "title": "Laufey Bewitched LP Orange Vinyl",
+        "price": 47.0
+    }, {
+        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
+        "title": "Coldplay Vinyl",
+        "price": 60.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
+        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
+        "price": 55.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
+        "title": "Laufey Bewitched LP Orange Vinyl",
+        "price": 47.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
+        "title": "Coldplay Vinyl",
+        "price": 60.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
+        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
+        "price": 55.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
+        "title": "Laufey Bewitched LP Orange Vinyl",
+        "price": 47.0
+    }
+]
