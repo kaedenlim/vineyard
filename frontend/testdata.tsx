c6400e0a22a1fc7a3e1fabb2427832f0ba82eff5
FQTeo <fuqiangteo@gmail.com>
2025-02-23 00:34:52 +0800

    update route and test data

--- frontend/testdata.tsx
+++ frontend/testdata.tsx
@@ -1,55 +1,56 @@
 export const testdata = [
     {
-        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
-        "title": "Coldplay Vinyl",
-        "price": 60.0
+        "link": "https://www.carousell.sg/p/naruko-tea-tree-anti-acne-sunscreen-spf50-30ml-1343214800/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/naruko_tea_tree_antiacne_sunsc_1734916912_3107674c_progressive_thumbnail.jpg",
+        "title": "NARUKO Tea Tree Anti-Acne Sunscreen SPF50  (30ml)",
+        "price": 12.0
     },
     {
-        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
-        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
-        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
-        "price": 55.0
+        "link": "https://www.carousell.sg/p/bnib-anessa-perfect-uv-sunscreen-skincare-milk-spf50-pa-for-face-body-60ml-1343262678/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/anessa_perfect_uv_sunscreen_sk_1734935925_d7f36502_progressive_thumbnail.jpg",
+        "title": "BNIB Anessa Perfect UV Sunscreen Skincare Milk SPF50+ PA++++ (For face & Body) 60ml",
+        "price": 30.0
     },
     {
-        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
-        "title": "Laufey Bewitched LP Orange Vinyl",
-        "price": 47.0
-    }, {
-        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
-        "title": "Coldplay Vinyl",
-        "price": 60.0
+        "link": "https://www.carousell.sg/p/skin-aqua-physical-sunscreen-spf50-pa-50ml-1343213735/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/skin_aqua_physical_sunscreen_s_1734916211_9e2266ad_progressive_thumbnail.jpg",
+        "title": "Skin Aqua Physical Sunscreen SPF50+/PA++++ [50ml]",
+        "price": 19.0
     },
     {
-        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
-        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
-        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
-        "price": 55.0
+        "link": "https://www.carousell.sg/p/naruko-tea-tree-anti-acne-sunscreen-spf50-30ml-1343214800/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/naruko_tea_tree_antiacne_sunsc_1734916912_3107674c_progressive_thumbnail.jpg",
+        "title": "NARUKO Tea Tree Anti-Acne Sunscreen SPF50  (30ml)",
+        "price": 12.0
     },
     {
-        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
-        "title": "Laufey Bewitched LP Orange Vinyl",
-        "price": 47.0
+        "link": "https://www.carousell.sg/p/bnib-anessa-perfect-uv-sunscreen-skincare-milk-spf50-pa-for-face-body-60ml-1343262678/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/anessa_perfect_uv_sunscreen_sk_1734935925_d7f36502_progressive_thumbnail.jpg",
+        "title": "BNIB Anessa Perfect UV Sunscreen Skincare Milk SPF50+ PA++++ (For face & Body) 60ml",
+        "price": 30.0
     },
     {
-        "link": "https://www.carousell.sg/p/coldplay-vinyl-1354087974/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/20/coldplay_vinyl_1740053161_4dd150e0_progressive_thumbnail.jpg",
-        "title": "Coldplay Vinyl",
-        "price": 60.0
+        "link": "https://www.carousell.sg/p/skin-aqua-physical-sunscreen-spf50-pa-50ml-1343213735/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/skin_aqua_physical_sunscreen_s_1734916211_9e2266ad_progressive_thumbnail.jpg",
+        "title": "Skin Aqua Physical Sunscreen SPF50+/PA++++ [50ml]",
+        "price": 19.0
     },
     {
-        "link": "https://www.carousell.sg/p/lady-gaga-\u2013-mayhem-vinyl-lp-cd-1350264510/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
-        "image": "https://media.karousell.com/media/photos/products/2025/1/30/lady_gaga__mayhem_vinyl_lp_1738215160_2daa4af6_progressive_thumbnail",
-        "title": "Lady Gaga \u2013 MAYHEM Vinyl LP / CD",
-        "price": 55.0
+        "link": "https://www.carousell.sg/p/naruko-tea-tree-anti-acne-sunscreen-spf50-30ml-1343214800/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=0",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/naruko_tea_tree_antiacne_sunsc_1734916912_3107674c_progressive_thumbnail.jpg",
+        "title": "NARUKO Tea Tree Anti-Acne Sunscreen SPF50  (30ml)",
+        "price": 12.0
     },
     {
-        "link": "https://www.carousell.sg/p/laufey-bewitched-lp-orange-vinyl-1353183130/?t-id=EuR1Sae4DS_1740206525201&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=45H1sN5Ca-PAaC5P&t-referrer_search_query=vinyl&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
-        "image": "https://media.karousell.com/media/photos/products/2025/2/15/laufey_bewitched_lp_orange_vin_1739633183_c21e5c08_progressive_thumbnail.jpg",
-        "title": "Laufey Bewitched LP Orange Vinyl",
-        "price": 47.0
+        "link": "https://www.carousell.sg/p/bnib-anessa-perfect-uv-sunscreen-skincare-milk-spf50-pa-for-face-body-60ml-1343262678/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=1",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/anessa_perfect_uv_sunscreen_sk_1734935925_d7f36502_progressive_thumbnail.jpg",
+        "title": "BNIB Anessa Perfect UV Sunscreen Skincare Milk SPF50+ PA++++ (For face & Body) 60ml",
+        "price": 30.0
+    },
+    {
+        "link": "https://www.carousell.sg/p/skin-aqua-physical-sunscreen-spf50-pa-50ml-1343213735/?t-id=XTJAKV61W6_1740241617715&t-referrer_browse_type=search_results&t-referrer_page_type=search&t-referrer_request_id=JybcIOLP2nXH1vV_&t-referrer_search_query=sunscreen&t-referrer_search_query_source=direct_search&t-referrer_sort_by=popular&t-tap_index=2",
+        "image": "https://media.karousell.com/media/photos/products/2024/12/23/skin_aqua_physical_sunscreen_s_1734916211_9e2266ad_progressive_thumbnail.jpg",
+        "title": "Skin Aqua Physical Sunscreen SPF50+/PA++++ [50ml]",
+        "price": 19.0
     }
 ]
