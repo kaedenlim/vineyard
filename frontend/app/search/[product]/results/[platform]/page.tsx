2bc88e09c903f3388e6eb390affa5f94531bb835
FQTeo <fuqiangteo@gmail.com>
2025-02-23 00:34:52 +0800

    update route and test data

--- frontend/app/search/[product]/results/[platform]/page.tsx
+++ frontend/app/search/[product]/results/[platform]/page.tsx
@@ -9,9 +9,10 @@ import { useParams } from "next/navigation";
 export default function SearchResults() {
     const params = useParams();
     const productQuery = params.product; // Access the [query] parameter
+    const productPlatform = params.platform;
     return (
         <div>
-            <div className="w-full h-[50px] p-8 text-lg">Search results for <span className="font-bold">{productQuery}</span>:</div>
+            <div className="w-full h-[50px] p-8 text-lg">Search results for <span className="font-bold">{productQuery}</span> from <span className="font-bold">{productPlatform}</span>:</div>
             <div className="flex flex-wrap justify-center items-center gap-4">
                 {testdata.map((item) => (
                     <ProductCard {...item} />
