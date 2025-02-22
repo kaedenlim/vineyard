18aa46073f91539c37a09cb9649c279c40e14ca6
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-02-22 15:47:25 +0800

    feat: add axiosclient for scrape endpoint

---
+++ frontend/services/axiosClient.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const axiosClient = axios.create({
+    // need change this to env var
+    baseURL: 'http://127.0.0.1:8000/',
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+// Response interceptor for handling errors globally
+axiosClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      return Promise.reject(error);
+    },
+  );
+  
+  export default axiosClient;
