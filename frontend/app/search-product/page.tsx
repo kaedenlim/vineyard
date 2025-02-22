0e780735cba657f164b74688034a8314de73dde7
Vince Tan <vince.tanyy@gmail.com>
2025-02-22 23:05:21 +0800

    feat: add search product screen

---
+++ frontend/app/search-product/page.tsx
@@ -0,0 +1,5 @@
+import SearchProduct from './searchProduct'
+
+export default function Page() {
+  return <SearchProduct />
+}
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.lazcdn.com',
+          port: '',
+          pathname: '/**', // Allow any path on this hostname
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
