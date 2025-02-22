9fb946c15ffbc6295cbc7ad8c2dc1e320dc74127
FQTeo <fuqiangteo@gmail.com>
2025-02-23 00:29:33 +0800

    add link from search product to search results

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-      domains: ['media.karousell.com'], // Allow images from this domain
+      domains: ['media.karousell.com', 'img.lazcdn.com'], // Allow images from this domain
     },
 };
