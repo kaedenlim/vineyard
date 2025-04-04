5e52a9a526bfbd6f0ad4ea2c3a485aebc284f5fc
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-04 22:25:36 +0800

    refactor: move dtos to one

---
+++ backend/models/dto.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, HttpUrl
+from typing import Optional
+
+class OnboardDTO(BaseModel):
+    shopee_url: Optional[HttpUrl] = ""  
+    lazada_url: Optional[HttpUrl] = ""
+    carousell_url: Optional[HttpUrl] = ""
