909022d8dacd8652f84cbc5dbe2ea11e98ddbe64
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-05 21:11:10 +0800

    oops

--- frontend/services/axiosClient.ts
+++ frontend/services/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosClient = axios.create({
     // need change this to env var
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'http://127.0.0.1:8000',
     headers: {
         "Content-Type": "application/json"
     }
