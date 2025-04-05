bce16bdeb24e94a9513c1f1e62d2f5e1812f6f5a
gavwyh <121929957+gavwyh@users.noreply.github.com>
2025-04-06 00:05:36 +0800

    feat: add username to dtos

--- backend/models/dto.py
+++ backend/models/dto.py
@@ -1,17 +1,15 @@
-from pydantic import BaseModel, HttpUrl, field_validator
+from pydantic import BaseModel, HttpUrl
 from typing import Optional
 
 class OnboardDTO(BaseModel):
-    shopee_url: Optional[HttpUrl] = None  
-    lazada_url: Optional[HttpUrl] = None
-    carousell_url: Optional[HttpUrl] = None
-
-    @field_validator("shopee_url", "lazada_url", "carousell_url", mode="before")
-    @classmethod
-    def empty_str_to_none(cls, value):
-        if value == "":
-            return None
-        return value
+    username: str 
+    shopee_url: Optional[HttpUrl] = ""  
+    lazada_url: Optional[HttpUrl] = ""
+    carousell_url: Optional[HttpUrl] = ""
 
 class ScrapeDTO(BaseModel):
+    username: str 
     product: str
+
+class DashboardDTO(BaseModel):
+    username: str
