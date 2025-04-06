15f5ce617513682a516454e489870d7ef352f936
FQTeo <fuqiangteo@gmail.com>
2025-04-06 12:17:32 +0800

    chore: remove pricing plans from nav

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -10,7 +10,6 @@ export default function Nav() {
         <div className="h-vh w-[15%] min-w-[150px] bg-[#f8f8f8] flex-col gap-0 px-6">
             <div className="h-[15vh] font-bold text-2xl py-6">Vineyard</div>
             <div className="h-[60vh] flex flex-col justify-center items-start gap-6 flex-grow py-6">
-            <div className="h-[60vh] flex flex-col justify-center items-start gap-6 flex-grow py-6">
                 <Link href="/search">
                     <div className={`text-lg ${pathname.startsWith("/search") ? "font-bold" : ""}`}>Search</div>
                 </Link>
@@ -20,11 +19,6 @@ export default function Nav() {
                 <Link href="/history">
                     <div className={`text-lg ${pathname.startsWith("/history") ? "font-bold" : ""}`}>History</div>
                 </Link>
-            </div>
-            </div>
-            <div className="h-[25vh] mt-auto flex flex-col justify-end items-start gap-1 py-6">
-                <div className="font-medium text-lg">View Pricing & Plans</div>
-                <div className="text-gray-500 text-sm line-clamp-1">Checkout which plans or subscriptions suit you best</div>
             </div>
         </div>
     )
